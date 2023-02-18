662ed3a1211418c948b220410a0439514df71410
Jazz <jazz@info-in.net>
2023-02-18 13:18:35 +0800

    Update to use argparse and allow single post option

--- helper.py
+++ helper.py
@@ -1,9 +1,12 @@
 import wget
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-def get_links(driver, query):
-    driver.get("https://www.micmicidol.com/search?q=" + query + "&max-results=10")
-    
-    """Check if empty result"""
+def get_links(driver, query, max_results):
+    driver.get("https://www.micmicidol.club/search?q=" + query + "&max-results=" + max_results)
+    handle_alert(driver)
+    # Check if empty result
     try:
         driver.find_elements("class name", "date-outer")
     except:
@@ -18,6 +21,7 @@ def get_links(driver, query):
 
 def get_image_link(driver, link):
     driver.get(link)
+    handle_alert(driver)
     content = driver.find_elements("xpath", "//div[@class='post-body entry-content']//a[@href]")
     image_links = []
     for a_tag in content:
@@ -25,9 +29,54 @@ def get_image_link(driver, link):
         image_links.append(link)
     return image_links
 
-def download_image(folder_path, destination, image):
+def make_folderpath(os, foldername):
+    image_folder = os.path.realpath('images')
+    # Create Images folder if not exist
+    if (not os.path.exists(image_folder)):
+        os.mkdir(image_folder)
+        
+    # Create path from query and url
+    folder_path = os.path.join(image_folder, foldername)
+    if (not os.path.exists(folder_path)):
+        os.mkdir(folder_path)
+    
+    return folder_path
+
+def download_image(driver, links, os, folder_path):
+    for link in links:
+        print(f'\nWorking on {link} ⌛️')
+        filename = os.path.split(link)[1]
+        filename = filename.replace('.html', '')
+        
+        base_destination = os.path.join(folder_path, filename)
+        
+        image_links = get_image_link(driver, link)
+        image_count = len(image_links)
+        print(f'\nFound {image_count} images. Start downloading... ⬇️')
+        
+        image_index = 1
+        for image in image_links:
+            print(f'\n{image_index} / ' + str(len(image_links)))
+            destination = base_destination + f' ({image_index})'
+            # Get image extension
+            ext = os.path.splitext(image)[1]
+            if not ext:
+                ext = '.jpg'
+            destination = destination + ext
+            try:
+                wget.download(image, out=destination)
+                print('\nDownloaded image to ' + folder_path + ' ✅')
+            except:
+                print('Image at ' + image + ' couldn\'t be downloaded')
+            image_index += 1
+        
+def handle_alert(driver):
     try:
-        wget.download(image, out=destination)
-        print('\nDownloaded image to ' + folder_path + ' ✅')
-    except:
-        print('Image at ' + image + ' couldn\'t be downloaded')
+        WebDriverWait(driver, 3).until(EC.alert_is_present(),
+                                    'Timed out waiting for ' +
+                                    'confirmation popup to appear.')
+        alert = driver.switch_to.alert
+        alert.accept()
+        print("Alert accepted.")
+    except TimeoutException:
+        print("No alert found. Proceeding...")
--- spider.py
+++ spider.py
@@ -1,3 +1,4 @@
+import argparse
 import os.path
 
 from selenium import webdriver
@@ -6,7 +7,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-from helper import get_links, get_image_link, download_image
+from helper import get_links, get_image_link, download_image, make_folderpath
+
+parser = argparse.ArgumentParser(prog= 'Micmicidol Scraper', description= 'Scrape images from micmicidol posts')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-q', '--query')
+parser.add_argument('-m', '--max-results', help= 'Only applicable with QUERY parameter', default=10)
+group.add_argument('-t', '--target-url')
+args = parser.parse_args()
+
+query = args.query
+target_url = args.target_url
+max_results = args.max_results
 
 # add options
 options = Options()
@@ -15,55 +27,40 @@ options.add_argument('headless');
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.implicitly_wait(1.5) # seconds
 
-while True:
-    query = ''
-    while not query:
-        query = input("Search by model/magazine name (Enter EXIT to close the program): ")
-    if query == 'EXIT':
-        break
-    
-    
+links = []
+
+if query:
     print('\nSearching... This could take awhile ⌛️')
     # get links
-    links = get_links(driver, query)
+    links = get_links(driver, query, max_results)
     if not links:
         print('No results found.')
     else:
-        """Found Results"""
+        # Found Results
         results_count = len(links)
         print(f'\nFound {results_count} result(s)')
         
-        image_folder = os.path.realpath('Images')
-        """Create Images folder if not exist"""
+        image_folder = os.path.realpath('images')
+        # Create Images folder if not exist
         if (not os.path.exists(image_folder)):
             os.mkdir(image_folder)
             
-        """Create path from query and url"""
+        # Create path from query and url
         folder_path = os.path.join(image_folder, query)
         if (not os.path.exists(folder_path)):
             os.mkdir(folder_path)
-
-        for link in links:
-            print(f'\nWorking on {link} ⌛️')
-            filename = os.path.split(link)[1]
-            filename = filename.replace('.html', '')
             
-            base_destination = os.path.join(folder_path, filename)
-            
-            image_links = get_image_link(driver, link)
-            image_count = len(image_links)
-            print(f'\nFound {image_count} images. Start downloading... ⬇️')
-            
-            image_index = 1
-            for image in image_links:
-                print(f'\n{image_index} / ' + str(len(image_links)))
-                destination = base_destination + f' ({image_index})'
-                """Get image extension"""
-                ext = os.path.splitext(image)[1]
-                if not ext:
-                    ext = '.jpg'
-                destination = destination + ext
-                download_image(folder_path, destination, image)
-                image_index += 1
-                
-driver.close()
+        download_image(driver, links, os, folder_path)
+    driver.close()
+else:
+    # get all images
+    image_links = get_image_link(driver, target_url)
+    
+    # use target_url as name 
+    filename = os.path.split(target_url)[1]
+    filename = filename.replace('.html', '')
+    folder_path = make_folderpath(os, filename)
+    
+    links.append(target_url)
+    download_image(driver, links, os, folder_path)
+    driver.close()
